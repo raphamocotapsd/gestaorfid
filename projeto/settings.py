406fd6e6ae398dc6e52e5c8863c21a6d97442e27
Raphael Costa <raphaelcostadeoliveira@gmail.com>
2023-08-24 10:41:43 -0300

    Atualização

--- projeto/settings.py
+++ projeto/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ebbnl5h(@ooy(lxwes*&pv0!ye1mt*g653fi8m2fd@j#%w@+#h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
